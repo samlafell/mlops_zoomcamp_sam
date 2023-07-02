eaadd000167f74f0eef204cc17c4088e94f20a01
Sam L <lafell.samuel@gmail.com>
2023-07-02 14:26:04 -0400

    homework updates

--- 05-monitoring/evidently_metrics_calculation.py
+++ 05-monitoring/evidently_metrics_calculation.py
@@ -13,7 +13,7 @@ from prefect import task, flow
 
 from evidently.report import Report
 from evidently import ColumnMapping
-from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
+from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric, ColumnQuantileMetric
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(message)s")
 
@@ -26,7 +26,10 @@ create table dummy_metrics(
 	timestamp timestamp,
 	prediction_drift float,
 	num_drifted_columns integer,
-	share_missing_values float
+	share_missing_values float,
+	fare_amt_25pct float,
+	fare_amt_50pct float,
+	fare_amt_75pct float
 )
 """
 
@@ -37,9 +40,9 @@ with open('models/lin_reg.bin', 'rb') as f_in:
 	model = joblib.load(f_in)
 
 # Simulate production usage, we are going to read our data day by day
-raw_data = pd.read_parquet('data/green_tripdata_2022-02.parquet')
+raw_data = pd.read_parquet('data/green_tripdata_2023-03.parquet')
 # Know where to start
-begin = datetime.datetime(2022, 2, 1, 0, 0)
+begin = datetime.datetime(2023, 3, 1, 0, 0)
 num_features = ['passenger_count', 'trip_distance', 'fare_amount', 'total_amount']
 cat_features = ['PULocationID', 'DOLocationID']
 
@@ -55,7 +58,10 @@ column_mapping = ColumnMapping(
 report = Report(metrics = [
     ColumnDriftMetric(column_name='prediction'),
     DatasetDriftMetric(),
-    DatasetMissingValuesMetric()
+    DatasetMissingValuesMetric(),
+    ColumnQuantileMetric(column_name='fare_amount', quantile=.25),
+    ColumnQuantileMetric(column_name='fare_amount', quantile=.5),
+    ColumnQuantileMetric(column_name='fare_amount', quantile=.75)
 ])
 
 @task
@@ -67,7 +73,7 @@ def prep_db():
 		with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example") as conn:
 			conn.execute(create_table_statement)
 
-@task
+@task(retries=2, retry_delay_seconds=5, name="calculate metrics")
 def calculate_metrics_postgresql(curr, i):
 	"""_summary_
 
@@ -77,31 +83,34 @@ def calculate_metrics_postgresql(curr, i):
 	"""
     # Pick up the date of interest
 	current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
-		(raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
+                         (raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]
 
 	#current_data.fillna(0, inplace=True)
 	current_data['prediction'] = model.predict(current_data[num_features + cat_features].fillna(0))
-
-	report.run(reference_data = reference_data, current_data = current_data,
-		column_mapping=column_mapping)
-
+ 	
+	report.run(reference_data = reference_data, current_data = current_data, column_mapping=column_mapping)
+ 
 	result = report.as_dict()
-
+ 	
 	prediction_drift = result['metrics'][0]['result']['drift_score']
 	num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
 	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+	fare_amt_25pct = result['metrics'][3]['result']['current']['value']
+	fare_amt_50pct = result['metrics'][4]['result']['current']['value']
+	fare_amt_75pct = result['metrics'][5]['result']['current']['value']
+  
 
 	curr.execute(
-		"INSERT INTO dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
-		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
+		"INSERT INTO dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values, fare_amt_25pct, fare_amt_50pct, fare_amt_75pct) values (%s, %s, %s, %s, %s, %s, %s)",
+		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values, fare_amt_25pct, fare_amt_50pct, fare_amt_75pct)
 	)
 
 @flow
 def batch_monitoring_backfill():
 	prep_db()
-	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
+	last_send = datetime.datetime.now() - datetime.timedelta(seconds=5)
 	with psycopg.connect("host=localhost port=5432 dbname=test user=postgres password=example", autocommit=True) as conn:
-		for i in range(27):
+		for i in range(30):
 			with conn.cursor() as curr:
 				calculate_metrics_postgresql(curr, i)
 
