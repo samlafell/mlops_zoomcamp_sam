f5b1faf63ad4c1a1907ce916bbfeea8f69e0e817
Sam L <lafell.samuel@gmail.com>
2023-07-26 09:09:01 -0400

    A ton of good work done today. Makefile and Scripts

--- 07-project/src/data/make_dataset.py
+++ 07-project/src/data/make_dataset.py
@@ -4,6 +4,78 @@ from pathlib import Path
 
 import click
 from dotenv import find_dotenv, load_dotenv
+import polars as pl
+import polars.selectors as cs
+
+def import_data(df_path: Path) -> pl.DataFrame():
+    """Import Data
+
+    Args:
+        train_df (pathlib.Path): Give path of data
+
+    Returns:
+        df (polars.DataFrame): Return Polars Dataframe
+    """
+    return pl.read_csv(df_path)
+
+
+def export_data(output_path, df):
+    """
+    Export Data
+    
+    Args:
+        output_path (pathlib.Path): Give path of data
+        df (polars.DataFrame): Return Polars Dataframe
+        
+    Returns:
+        None
+    """
+    with open(output_path, 'w') as f:
+        df.write_csv(f)
+    
+
+class Preprocessor:
+    def __init__(self, data):
+        self.data = data
+
+    def shuffle_data(self):
+        ids_shuffled = self.data.select(pl.col('Id').shuffle(seed=42))
+        self.data = self.data.join(ids_shuffled, how = 'inner', on = 'Id')
+        return self
+
+    def split_data(self):
+        data_rows = self.data.shape[0]
+        self.data_train = self.data.slice(0, int(data_rows * 0.7))
+        self.data_val = self.data.slice(int(data_rows * 0.7), int(data_rows * 0.15))
+        self.data_test = self.data.slice(int(data_rows * 0.85), data_rows)
+        return self
+
+    def split_X_y(self):
+        self.X_train = self.data_train.drop('quality', 'Id')
+        self.y_train = self.data_train.select('quality')
+
+        self.X_val = self.data_val.drop('quality', 'Id')
+        self.y_val = self.data_val.select('quality')
+
+        self.X_test = self.data_test.drop('quality', 'Id')
+        self.y_test = self.data_test.select('quality')
+
+        return self
+
+    def standardize_data(self):
+        """
+        This is done to make sure that the data is centered around 0 and has a standard deviation of 1
+        Polars does not have a built in standardization function, so we have to do it manually
+        This is done separately for each dataset to avoid data leakage        
+        """
+        for dataset in [self.X_train, self.X_val, self.X_test]:
+            for col_name in dataset.columns:
+                col_mean = dataset[col_name].mean()
+                col_std = dataset[col_name].std()
+                dataset = dataset.with_columns(((dataset[col_name] - col_mean) / col_std).alias(f'{col_name}_std'))
+            dataset = dataset.lazy().select(cs.ends_with('std')).collect()
+
+        return self
 
 
 @click.command()
@@ -15,7 +87,42 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    
+    # Import data
+    logger.info(f'input filepath is {input_filepath}')
+    data = import_data(df_path = Path(input_filepath, 'WineQT.csv').resolve())
 
+    # Run Preprocessing
+    preprocessor = Preprocessor(data)
+    preprocessor.shuffle_data().split_data().split_X_y().standardize_data()
+    logger.info('Initialized Preprocessor')
+
+    X_train = preprocessor.X_train
+    y_train = preprocessor.y_train
+
+    X_val = preprocessor.X_val
+    y_val = preprocessor.y_val
+
+    X_test = preprocessor.X_test
+    y_test = preprocessor.y_test
+    logger.info('Finished Preprocessing')
+    
+    data_dict = {
+        'X_train': X_train,
+        "y_train": y_train,
+        "X_val": X_val,
+        "y_val": y_val,
+        "X_test": X_test,
+        "y_test": y_test
+    }
+
+    logger.info('Saving data to csv')
+    for dataset_name, dataset in data_dict.items():
+        export_data(output_path = Path(output_filepath, f'{dataset_name}.csv'), df = dataset)
+    logger.info('Finished saving data to csv')
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+    
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
---
+++ 07-project/src/model_code/train_model.py
@@ -0,0 +1,142 @@
+import logging
+from pathlib import Path
+import click
+from dotenv import find_dotenv, load_dotenv
+import polars as pl
+# More Models
+import xgboost as xgb
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+from hyperopt.pyll import scope
+from typing import Dict, Any
+from numpy import ndarray
+from xgboost import DMatrix
+import mlflow
+from sklearn.metrics import mean_squared_error
+
+# Set MLFlow Tracking URI
+logging.info(f'tracking uri before defining: {mlflow.get_tracking_uri()}')
+mlflow.set_tracking_uri("http://localhost:5001")
+logging.info(f'tracking uri: {mlflow.get_tracking_uri()}')
+# Set Experiment
+mlflow.set_experiment('wine_dataset')
+
+class ImportData():
+    def __init__(self, data_filepath):
+        self.data_path = Path(data_filepath)
+        
+    def set_paths(self):
+        self.X_train_path = self.data_path / 'X_train.csv'
+        self.y_train_path = self.data_path / 'y_train.csv'
+        
+        self.X_val_path = self.data_path / 'X_val.csv'
+        self.y_val_path = self.data_path / 'y_val.csv'
+        
+        return self
+    
+    def import_data(self):
+        X_train = pl.read_csv(self.X_train_path)
+        y_train = pl.read_csv(self.y_train_path)
+        X_val = pl.read_csv(self.X_val_path)
+        y_val = pl.read_csv(self.y_val_path)
+        return X_train, y_train, X_val, y_val
+
+
+def convert_to_numpy(**args):
+    result = {}
+    for arg_name, arg in args.items():
+        if isinstance(arg, pl.DataFrame):
+            result[arg_name] = arg.to_numpy().flatten() if arg.shape[1] == 1 else arg.to_numpy()
+        else:
+            result[arg_name] = arg
+    return result.values()
+
+
+@click.command()
+@click.argument('data_filepath', type=click.Path(exists=True))
+@click.argument('early_stopping_rounds', type=click.INT)
+def main(data_filepath):
+    """
+    Training Model
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('training model')
+    
+    # Initialize preprocess class
+    importing = ImportData(data_filepath)
+    importing.set_paths()
+    # Load in X and y's
+    X_train, y_train, X_val, y_val = importing.import_data()
+    X_train_np, y_train_np, X_val_np, y_val_np = convert_to_numpy(X_train=X_train, 
+                                                      y_train=y_train,
+                                                      X_val=X_val,
+                                                      y_val=y_val)
+    
+    # Train Model
+    search_space = {
+        'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+        'learning_rate': hp.loguniform('learning_rate', -3, 0),
+        'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+        'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+        'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+        'objective': 'multi:softmax',
+        'num_class': 9,
+        'seed': 42
+    }
+
+    logging.info('passing to DMatrix')
+    train = xgb.DMatrix(data=X_train_np, label = y_train_np)
+    valid = xgb.DMatrix(data=X_val_np, label = y_val_np)
+
+    def objective(params: Dict[str, Any]) -> Dict[str, Any]:
+        '''
+        Train an XGBoost model with given parameters and datasets, log the training 
+        process with MLFlow, make predictions on the validation set, calculate RMSE 
+        and log it with MLFlow. Return the RMSE and the status.
+
+        Parameters:
+        params (dict): A dictionary of parameters to use for the XGBoost model.
+
+        Returns:
+        dict: A dictionary with 'loss' key indicating the root mean squared error (RMSE) 
+        on validation set and 'status' key indicating the status of the function.
+        '''
+        logging.info('inside objective function')
+        with mlflow.start_run():
+            mlflow.set_tag("model", "xgboost")
+            mlflow.log_params(params)
+            mlflow.xgboost.autolog()
+            booster = xgb.train(
+                params=params,
+                dtrain=train,
+                num_boost_round=1000,
+                evals=[(valid, 'validation')],
+                early_stopping_rounds=early_stopping_rounds,
+            )
+            y_pred = booster.predict(valid)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
+
+        return {'loss': rmse, 'status': STATUS_OK}
+    
+    logging.info('starting hyperparameter search')
+    best_result = fmin(
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=50,
+        trials=Trials()
+    )
+    
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+
+    main()
